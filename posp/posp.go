1931fb67f94d1f18bdb22893800753878012645f
Ad van der Veer <advanderveer@gmail.com>
2022-02-10 15:04:07 +0100

    with portable only

--- posp/posp.go
+++ posp/posp.go
@@ -6,9 +6,6 @@ package posp
 // #include "../src/b3/blake3.c"
 // #include "../src/b3/blake3_portable.c"
 // #include "../src/b3/blake3_dispatch.c"
-// #include "../src/b3/blake3_avx2_x86-64_unix.S"
-// #include "../src/b3/blake3_avx512_x86-64_unix.S"
-// #include "../src/b3/blake3_sse41_x86-64_unix.S"
 // #include "../src/chacha8.c"
 import "C"
 
